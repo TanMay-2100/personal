68ab4949ef784f2015218c6c4471caf46e9e9d11
TanMay-2100 <tanmay.sharma@staqo.com>
2026-03-02 14:53:52 +0530

    pro

---
+++ project1/script.js
@@ -0,0 +1,13 @@
+function showWish(option) {
+    const messageBox = document.getElementById("messageBox");
+
+    const wishes = {
+        1: "🌸 May your life be filled with vibrant colors and happiness. Happy Holi!",
+        2: "🎨 Let’s splash colors of joy and celebrate together. Happy Holi my friend!",
+        3: "🌈 May this Holi bring love, peace, and prosperity to your life!",
+        4: "💖 Wishing you and your family a bright and colorful Holi!",
+        5: "✨ Celebrate this festival of colors with lots of fun and laughter. Happy Holi!"
+    };
+
+    messageBox.innerText = wishes[option];
+}
